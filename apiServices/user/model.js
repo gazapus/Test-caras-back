ef4601f7a724d61f1edfeb470120882ba9fe5c6d
Cristian Villafane <cristianv129@hotmail.com>
2021-03-02 18:49:01 -0300

    Agregado post middleware al modelo de Group

--- apiServices/user/model.js
+++ apiServices/user/model.js
@@ -1,15 +1,14 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+let Group = require('../group/model');
 
 let schema = new Schema({
     name: String,
     lastname: String,
     email: String,
     password: String,
-    confirmed: {
-        type: Boolean,
-        default: false
-    }
+    confirmed: { type: Boolean, default: false },
+    groups: [{ type: Schema.ObjectId, ref: 'Group' }]
 },
     { timestamps: true }
 );
@@ -19,5 +18,17 @@ schema.method("toJSON", function () {
     object.id = _id;
     return object;
 });
+
+schema.post('save', async function(doc, next) {
+    if(doc.groups.length > 0 ) return next();
+    try {
+        let universalGroup = new Group({description: 'universal', owner: doc._id})
+        await universalGroup.save();
+        doc.groups.push(universalGroup._id);
+        await doc.save();
+    } catch(err) {
+        console.log(err);
+    }
+})
 
 module.exports = mongoose.model('User', schema)
